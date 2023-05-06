630f0a8c51266bdb7761339fd7562ba5277fbf5a
Andrés López <andres.lp.dev@gmail.com>
2023-05-05 22:53:43 -0500

    Se cambio la forma en la que se instaciaban las clases de nuestro controlador y se realizo de una forma mas sostenible, sin el if infinito que se implemento con anterioridad

--- src/main/java/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -2,12 +2,7 @@ package com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 
-import com.alura.gerenciador.accion.EliminarEmpresa;
-import com.alura.gerenciador.accion.ListaEmpresas;
-import com.alura.gerenciador.accion.ModificarEmpresa;
-import com.alura.gerenciador.accion.MostrarEmpresa;
-import com.alura.gerenciador.accion.NuevaEmpresa;
-import com.alura.gerenciador.accion.NuevaEmpresaForm;
+import com.alura.gerenciador.accion.Accion;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -24,44 +19,21 @@ public class UnicaEntradaServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String paramAccion = request.getParameter("accion");
-		String nombre = null;
 		
 		String nombreClase = "com.alura.gerenciador.accion."+paramAccion;
+		String nombre = null;
 		
-		
-		if(paramAccion.equals("ListaEmpresas")) {
-			
-			ListaEmpresas accion = new ListaEmpresas();
-			nombre = accion.ejecutar(request, response);
-			
-		}else if(paramAccion.equals("MostrarEmpresa")) {
-			
-			MostrarEmpresa accion =new MostrarEmpresa();
-			nombre = accion.ejecutar(request, response);
-			
-		}else if(paramAccion.equals("EliminarEmpresa")) {
-			
-			EliminarEmpresa accion =new EliminarEmpresa();
-			nombre = accion.ejecutar(request, response);
-			
-		}else if(paramAccion.equals("ModificarEmpresa")) {
-			
-			ModificarEmpresa accion =new ModificarEmpresa();
-			nombre = accion.ejecutar(request, response);
-			
-		}else if(paramAccion.equals("NuevaEmpresa")) {
-			
-			NuevaEmpresa accion =new NuevaEmpresa();
-			nombre = accion.ejecutar(request, response);
-			
-		}else if(paramAccion.equals("NuevaEmpresaForm")) {
-			
-			NuevaEmpresaForm accion =new NuevaEmpresaForm();
-			nombre = accion.ejecutar(request, response);
+		try {
+			Class clase = Class.forName(nombreClase);
+			Accion accion = (Accion) clase.newInstance();
+			nombre = accion.ejecutar(request,response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ServletException
+				| IOException	e) {
+			throw new ServletException(e);
 		}
 		
-		
 		String[] tipoYDireccion = nombre.split(":");
+		
 		if(tipoYDireccion[0].equals("forward")) {
 			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/"+tipoYDireccion[1]);
 			rd.forward(request, response);
@@ -69,9 +41,37 @@ public class UnicaEntradaServlet extends HttpServlet {
 			response.sendRedirect(tipoYDireccion[1]);
 		}
 		
-		
-		
-		
+//		String nombre = null;
+//		if(paramAccion.equals("ListaEmpresas")) {
+//			
+//			ListaEmpresas accion = new ListaEmpresas();
+//			nombre = accion.ejecutar(request, response);
+//			
+//		}else if(paramAccion.equals("MostrarEmpresa")) {
+//			
+//			MostrarEmpresa accion =new MostrarEmpresa();
+//			nombre = accion.ejecutar(request, response);
+//			
+//		}else if(paramAccion.equals("EliminarEmpresa")) {
+//			
+//			EliminarEmpresa accion =new EliminarEmpresa();
+//			nombre = accion.ejecutar(request, response);
+//			
+//		}else if(paramAccion.equals("ModificarEmpresa")) {
+//			
+//			ModificarEmpresa accion =new ModificarEmpresa();
+//			nombre = accion.ejecutar(request, response);
+//			
+//		}else if(paramAccion.equals("NuevaEmpresa")) {
+//			
+//			NuevaEmpresa accion =new NuevaEmpresa();
+//			nombre = accion.ejecutar(request, response);
+//			
+//		}else if(paramAccion.equals("NuevaEmpresaForm")) {
+//			
+//			NuevaEmpresaForm accion =new NuevaEmpresaForm();
+//			nombre = accion.ejecutar(request, response);
+//		}
 		
 	}
 
